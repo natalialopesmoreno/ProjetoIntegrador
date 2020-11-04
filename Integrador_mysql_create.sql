933a75b6a4ec0f9fa9aaeb0154e1d108bbfb577c
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2020-11-04 15:33:31 -0300

    Update Integrador_mysql_create.sql

--- Integrador_mysql_create.sql
+++ Integrador_mysql_create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `Postagem` (
-	`id_postagem` BINARY NOT NULL AUTO_INCREMENT,
+	`id_postagem` INT NOT NULL AUTO_INCREMENT,
 	`id_usuario` INT NOT NULL,
 	`id_tema` INT NOT NULL,
 	`titulo` varchar(100) NOT NULL,
@@ -23,6 +23,18 @@ CREATE TABLE `Usuario` (
 	`email` varchar(20) NOT NULL,
 	`senha` varchar(15) NOT NULL,
 	PRIMARY KEY (`id_usuario`)
+);
+
+CREATE TABLE `Comentario` (
+	`id_comentario` BINARY NOT NULL AUTO_INCREMENT,
+	`id_usuario` INT NOT NULL,
+	`id_postagem` INT NOT NULL,
+	`artigo` TEXT(1000) NOT NULL,
+	`data` DATETIME NOT NULL DEFAULT '"yyyy-mm-dd"',
+	`curtir` BOOLEAN NOT NULL,
+	PRIMARY KEY (`id_comentario`),
+	CONSTRAINT `Postagem_fk2` FOREIGN KEY (`id_usuario`) REFERENCES `Usuario`(`id_usuario`),
+	CONSTRAINT `Postagem_fk3` FOREIGN KEY (`id_postagem`) REFERENCES `Postagem`(`id_postagem`)
 );
 
 ALTER TABLE `Postagem` ADD CONSTRAINT `Postagem_fk0` FOREIGN KEY (`id_usuario`) REFERENCES `Usuario`(`id_usuario`);
