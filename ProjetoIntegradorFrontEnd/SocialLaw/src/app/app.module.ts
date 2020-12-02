d8f6ac5099e1fbd02a8aba8e98a1d5c7bcb1bf55
Natália L. Moreno <63927404+natalialopesmoreno@users.noreply.github.com>
2020-12-02 16:49:44 -0300

    Front-End

--- ProjetoIntegradorFrontEnd/SocialLaw/src/app/app.module.ts
+++ ProjetoIntegradorFrontEnd/SocialLaw/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarLateralComponent } from './navbar-lateral/navbar-lateral.component';
+import { NavbarSuperiorComponent } from './navbar-superior/navbar-superior.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
+import { PesquisarComponent } from './pesquisar/pesquisar.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarLateralComponent,
+    NavbarSuperiorComponent,
+    FooterComponent,
+    HomeComponent,
+    FeedComponent,
+    PesquisarComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
